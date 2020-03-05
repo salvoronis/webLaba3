cecfe7dc98223c48dd7730a7e4c9e024fdca7fae
salvoroni <salvoronis@gmail.com>
2020-03-05 17:05:22 +0300

    fucking jsf

--- src/main/java/laba3/LabaBean.java
+++ src/main/java/laba3/LabaBean.java
@@ -6,24 +6,46 @@ import lombok.Data;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean
 @ViewScoped
 @Data
 public class LabaBean implements Serializable{
 	private MenuItem menuItem = new MenuItem();
+	private MenuItem imageItem = new MenuItem();
 
-	private String x;
-	private String y;
+	private String radius = "4";
 
 	private List<MenuItem> items = new ArrayList<MenuItem>();
 
+	public LabaBean(){
+		menuItem.setSlider(0);
+		menuItem.setSelect(this.radius);
+		imageItem.setSelect(this.radius);
+	}
+
+	public void change(ValueChangeEvent event){
+		String value = event.getNewValue().toString();
+		this.radius = value;
+		System.out.println(this.radius);
+	}
+
 	public void addItem(){
+		menuItem.setSelect(this.radius);
+		menuItem.normalizeForm();
 		items.add(menuItem);
 		menuItem = new MenuItem();
+		menuItem.setSlider(0);
 	}
 
 	public void suka(){
-		System.out.println(x + " " + y);
+		System.out.println(this.radius);
+		imageItem.setSelect(this.radius);
+		System.out.println(imageItem.getSlider() +" "+ imageItem.getInput()+" "+imageItem.getSelect());
+		System.out.println();
+		imageItem.normalizeItems();
+		items.add(imageItem);
+		imageItem = new MenuItem();
 	}
 }
--- src/main/java/laba3/MenuItem.java
+++ src/main/java/laba3/MenuItem.java
@@ -1,10 +1,44 @@
 package com.laba3;
 
 import lombok.Data;
+import java.lang.Math;
+
 
 @Data
 public class MenuItem{
-	private Integer slider;
-	private String input;
+	private Integer slider = 0;
+	private String input = "0";
 	private String select;
+
+	private double x;
+	private double y;
+	private double r;
+	private String result;
+
+	public void normalizeItems(){
+		this.x = ((double)slider-282.0)/50.0;
+		this.y = -(Float.parseFloat(input)-282.0)/50.0;
+		this.r = Float.parseFloat(select);
+		checkIt();
+	}
+
+	public void normalizeForm(){
+		System.out.println(slider);
+		this.y = Float.parseFloat(input);
+		this.x = slider.equals(null) ? 0.0 : (float)slider;
+		this.r = Float.parseFloat(select);
+		checkIt();
+	}
+
+	public void checkIt(){
+		if ((this.x >= 0.0) && (this.y >= 0.0) && (this.y <= -this.x/2.0 + this.r/2)) {
+			this.result = "Попадание";
+		} else if ((this.y <= 0.0) && (this.x >= 0.0) && (this.x <= this.r) && (this.y >= -this.r/2)){
+			this.result = "Попадание";
+		} else if ((y <= 0.0) && (x <= 0.0) && ((Math.pow((Math.abs(this.x)), 2) + Math.pow((Math.abs(this.y)), 2))<=Math.pow(this.r/2, 2))){
+			this.result = "Попадание";
+		} else {
+			this.result = "Промах";
+		}
+	}
 }
