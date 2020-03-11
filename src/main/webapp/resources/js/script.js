8ea0531d0d23ae38cbfce9dd754bb08c109caf94
salvoroni <salvoronis@gmail.com>
2020-03-11 23:43:15 +0300

    finally

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -6,6 +6,24 @@ $("svg").on("click",function(event){
 	//var data = event.pageX+','+event.pageY
 	//fuckingImg([{suka:event.pageX+','+event.pageY}]);
 });
-function hui(){
-	console.log("hui");
+function renderImg(value){
+	console.log(value);
+
+	value*=50;
+    var valh = value/2;
+    var a = value + 270;
+    var b = 270 - valh;
+    var c = 270 + valh
+    $('rect').attr('width', value);
+    $('rect').attr('height', valh);
+    $('#triangle').attr('points', '270,270 '+a+',270 270,'+b);
+    $('path').attr('d','M '+b+' 270 A 90 90, 0, 0, 0, 270 '+c+' L 270 270 Z');
+    $('.ypoint').each(function(index){
+    	var kuda = (index*valh)+(270-value);
+        $(this).attr('y',kuda);
+    });
+    $('.xpoint').each(function(index){
+        var kuda = (index*valh)+(270-value);
+        $(this).attr('x',kuda);
+    });
 }
