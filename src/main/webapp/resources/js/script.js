465ea5f7cf4c07e63d2885b20c0f128550ea35b0
salvoroni <salvoronis@gmail.com>
2020-03-26 13:59:30 +0300

    pre release

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,5 +1,8 @@
 $("svg").on("click",function(event){
-	fuckingImg([{name:'ximg',value:event.pageX},{name:'suka',value:event.pageY}]);
+    fuckingImg([{name:'ximg',value:event.pageX},{name:'suka',value:event.pageY}]);
+    window.getThisMotherfucker = function(xhr, status, args) {
+        alert(args.returnedValue);
+    }
 });
 function check() {
     if ($("#ytext").val() != '')
@@ -7,19 +10,10 @@ function check() {
     else
         $("#formjs").attr('disabled', 'false');
 }
-/*$("formjs").on("click", function(event){
-    console.log($("#ytext").val);
-    console.log(widget_helpme_sliderx.getValue());
-    //fuckingForm([{name:'xform',value:event.pageX},{name:'yform',value:event.pageY}]);
-});*/
-/*function formfuck(){
-    console.log($("#ytext").val());
-    console.log(widget_helpme_sliderx.getValue());
-}*/
 function renderImg(value){
-	console.log(value);
+    console.log(value);
 
-	value*=50;
+    value*=50;
     var valh = value/2;
     var a = value + 270;
     var b = 270 - valh;
@@ -29,7 +23,7 @@ function renderImg(value){
     $('#triangle').attr('points', '270,270 '+a+',270 270,'+b);
     $('path').attr('d','M '+b+' 270 A 90 90, 0, 0, 0, 270 '+c+' L 270 270 Z');
     $('.ypoint').each(function(index){
-    	var kuda = (index*valh)+(270-value);
+        var kuda = (index*valh)+(270-value);
         $(this).attr('y',kuda);
     });
     $('.xpoint').each(function(index){
